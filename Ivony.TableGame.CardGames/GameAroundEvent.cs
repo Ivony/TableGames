34905c79b12420267ab017da49fcaa823361b1bc
Ivony <Ivony@live.com>
2014-10-27 11:45:13 +0800

    增补注释

--- Ivony.TableGame.CardGames/GameAroundEvent.cs
+++ Ivony.TableGame.CardGames/GameAroundEvent.cs
@@ -6,14 +6,26 @@ using System.Threading.Tasks;
 
 namespace Ivony.TableGame.CardGames
 {
+
+
+  /// <summary>
+  /// 定义游戏回合事件
+  /// </summary>
   public class GameAroundEvent : GameEventBase, IGamePlayerEvent
   {
 
+    /// <summary>
+    /// 创建 GameAroundEvent 对象
+    /// </summary>
+    /// <param name="player">轮到出牌的玩家</param>
     public GameAroundEvent( CardGamePlayer player )
     {
       Player = player;
     }
 
+    /// <summary>
+    /// 获取当前轮到出牌的玩家
+    /// </summary>
     public GamePlayerBase Player
     {
       get;
