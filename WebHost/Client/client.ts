f0033d506a0d58659d6a1952d0b7768e6cacab30
ivony <ivony@IVONY-HOME>
2017-04-23 19:40:47 +0800

    更新组件

---
+++ WebHost/Client/client.ts
@@ -0,0 +1,17 @@
+﻿///
+
+export class client
+{
+
+  private host: string;
+
+  constructor(host: string)
+  {
+    this.host = host;
+  }
+
+  public async createRoom(name: string): Promise<void>
+  {
+  }
+
+}
